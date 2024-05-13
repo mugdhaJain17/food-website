82548464d26b2147b523cee63b11400ef0db4149
mugdhajain17 <mugdha1705@gmail.com>
2024-05-13 18:04:49 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+        <h2>Explore a tantalizing array of dishes waiting to delight your taste buds</h2>
+        <p>Indulge in culinary delights from the comfort of your home with our seamless food delivery service. Discover a world of flavors, from local favorites to global cuisines, delivered right to your doorstep. Ordering is easy, satisfaction is guaranteed, and every bite is a journey worth savoring.</p>
+        <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
